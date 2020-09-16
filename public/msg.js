aa57d2d6e385ea3c2746ec3ea370738ac0f49400
lyndoncruz <lyndoncruz@gmail.com>
2020-09-16 14:22:46 +0800

    Add files via upload

--- public/msg.js
+++ public/msg.js
@@ -19,7 +19,6 @@ $(document).ready(function(){
     $(window).keydown(function(event){
         if(event.keyCode == 13) {
           event.preventDefault();
-          return false;
         }
       });
 
